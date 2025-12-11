54180af8a2286a49d373ac9e76b40230299c9f7c
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2025-12-10 16:24:55 -0800

    use client

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import type { NavLinkProps } from "../types";
 
-export default function NavLink({ href, children }: NavLinkProps) {
+export default function NavLink({ href, children, isActive }: NavLinkProps) {
+    console.log(isActive)
     return (
         <li className="text-sm uppercase">
             <Link
                 href={href}
-                className="px-4 py-2 text-gray-700 transition-colors rounded-md cursor-pointer hover:text-blue-500"
+                className={`px-4 py-2 transition-colors rounded-md cursor-pointer hover:text-orange-accent ${isActive ? "text-orange-700" : "text-gray-700"}`}
             >{children}</Link>
         </li>
     )
